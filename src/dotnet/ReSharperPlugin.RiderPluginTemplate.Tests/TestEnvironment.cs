0517f80782b1accdf3d2af4361674abefa5b9202
Ivan Migalev <ivan.migalev@jetbrains.com>
2024-05-09 17:09:40 +0200

    Initial commit (deploy from the plugin template)

---
+++ src/dotnet/ReSharperPlugin.RiderPluginTemplate.Tests/TestEnvironment.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.Feature.Services;
+using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.ReSharper.TestFramework;
+using JetBrains.TestFramework;
+using JetBrains.TestFramework.Application.Zones;
+using NUnit.Framework;
+
+[assembly: Apartment(ApartmentState.STA)]
+
+namespace ReSharperPlugin.RiderPluginTemplate.Tests
+{
+    [ZoneDefinition]
+    public class RiderPluginTemplateTestEnvironmentZone : ITestsEnvZone, IRequire<PsiFeatureTestZone>, IRequire<IRiderPluginTemplateZone> { }
+
+    [ZoneMarker]
+    public class ZoneMarker : IRequire<ICodeEditingZone>, IRequire<ILanguageCSharpZone>, IRequire<RiderPluginTemplateTestEnvironmentZone> { }
+
+    [SetUpFixture]
+    public class RiderPluginTemplateTestsAssembly : ExtensionTestEnvironmentAssembly<RiderPluginTemplateTestEnvironmentZone> { }
+}

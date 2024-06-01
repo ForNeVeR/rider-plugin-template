ee651d1ee645df89af06ad253390a757fe7cde63
Ivan Migalev <ivan.migalev@jetbrains.com>
2024-06-01 19:04:06 +0200

    Template: drop an unused file

--- repack.cmd
+++
@@ -1,9 +0,0 @@
-:; set -eo pipefail
-:; SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
-:; git clean -fdx ${SCRIPT_DIR}
-:; dotnet pack -o ./output
-:; exit $?
-
-@echo off
-git clean -fdx %~dp0
-dotnet pack -o ./output
